85b6b8f236ed53536844da479fa8c13241476512
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-08-17 14:04:33 +0200

    test/qgraph: tpci200 test node
    Convert tests/tpci200-test in qgraph test node,
    tpci200-test. Since it's a nop test, node creation and
    initialization is made in the same file.
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>

--- tests/tpci200-test.c
+++ tests/tpci200-test.c
@@ -9,23 +9,65 @@
 
 #include "qemu/osdep.h"
 #include "libqtest.h"
+#include "libqos/qgraph.h"
+
+typedef struct QTpci200 QTpci200;
+typedef struct QIpack QIpack;
+
+struct QIpack {
+
+};
+struct QTpci200 {
+    QOSGraphObject obj;
+    QIpack ipack;
+};
 
 /* Tests only initialization so far. TODO: Replace with functional tests */
-static void nop(void)
+static void nop(void *obj, void *data, QGuestAllocator *alloc)
 {
 }
 
-int main(int argc, char **argv)
+/* tpci200 */
+static void *tpci200_get_driver(void *obj, const char *interface)
 {
-    int ret;
+    QTpci200 *tpci200 = obj;
+    if (!g_strcmp0(interface, "ipack")) {
+        return &tpci200->ipack;
+    }
 
-    g_test_init(&argc, &argv, NULL);
-    qtest_add_func("/tpci200/nop", nop);
-
-    qtest_start("-device tpci200");
-    ret = g_test_run();
-
-    qtest_end();
-
-    return ret;
+    fprintf(stderr, "%s not present in tpci200\n", interface);
+    g_assert_not_reached();
 }
+
+static void tpci200_destructor(QOSGraphObject *obj)
+{
+    QTpci200 *tpci200 = (QTpci200 *) obj;
+    g_free(tpci200);
+}
+
+static void *tpci200_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QTpci200 *tpci200 = g_new0(QTpci200, 1);
+    tpci200->obj.destructor = tpci200_destructor;
+    tpci200->obj.get_driver = tpci200_get_driver;
+
+    return &tpci200->obj;
+}
+
+static void tpci200_register_nodes(void)
+{
+    qos_node_create_driver("tpci200", tpci200_create);
+    qos_node_consumes("tpci200", "pci-bus", &(QOSGraphEdgeOptions) {
+        .extra_device_opts = "id=ipack0",
+    });
+    qos_node_produces("tpci200", "ipack");
+}
+
+libqos_init(tpci200_register_nodes);
+
+static void register_tpci200_test(void)
+{
+    qos_add_test("tpci200-test", "tpci200", nop, NULL);
+}
+
+libqos_init(register_tpci200_test);
