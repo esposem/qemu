761cd814db4fd9cbbfa57a91ee3f45b744739130
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-08-17 12:01:59 +0200

    test/qgraph: ac97 test node
    Convert tests/ac97-test in qgraph test node,
    ac97-test. Since it's a nop test, node creation and
    initialization is made in the same file.
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>

--- tests/ac97-test.c
+++ tests/ac97-test.c
@@ -9,23 +9,44 @@
 
 #include "qemu/osdep.h"
 #include "libqtest.h"
+#include "libqos/qgraph.h"
+
+typedef struct QAC97 QAC97;
+
+struct QAC97 {
+    QOSGraphObject obj;
+};
 
 /* Tests only initialization so far. TODO: Replace with functional tests */
-static void nop(void)
+static void nop(void *obj, void *data, QGuestAllocator *alloc)
 {
 }
 
-int main(int argc, char **argv)
+static void ac97_destructor(QOSGraphObject *obj)
 {
-    int ret;
-
-    g_test_init(&argc, &argv, NULL);
-    qtest_add_func("/ac97/nop", nop);
-
-    qtest_start("-device AC97");
-    ret = g_test_run();
-
-    qtest_end();
-
-    return ret;
+    QAC97 *ac97 = (QAC97 *)obj;
+    g_free(ac97);
 }
+
+static void *ac97_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QAC97 *ac97 = g_new0(QAC97, 1);
+    ac97->obj.destructor = ac97_destructor;
+
+    return &ac97->obj;
+}
+
+static void ac97_register_nodes(void)
+{
+    qos_node_create_driver("AC97", ac97_create);
+    qos_node_consumes("AC97", "pci-bus", NULL);
+}
+
+libqos_init(ac97_register_nodes);
+
+static void register_ac97_test(void)
+{
+    qos_add_test("ac97-test", "AC97", nop, NULL);
+}
+
+libqos_init(register_ac97_test);
