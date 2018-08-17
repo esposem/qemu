01e22e61c43fdf4ada48529574d0e34f02d62129
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-08-17 15:52:52 +0200

    test/qgraph: vmxnet3 test node
    Convert tests/vmxnet3-test in qgraph test node,
    vmxnet3-test. Since it's a nop test, node creation and
    initialization is made in the same file.
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>

--- tests/vmxnet3-test.c
+++ tests/vmxnet3-test.c
@@ -9,23 +9,44 @@
 
 #include "qemu/osdep.h"
 #include "libqtest.h"
+#include "libqos/qgraph.h"
+
+typedef struct QVmxnet3 QVmxnet3;
+
+struct QVmxnet3 {
+    QOSGraphObject obj;
+};
 
 /* Tests only initialization so far. TODO: Replace with functional tests */
-static void nop(void)
+static void nop(void *obj, void *data, QGuestAllocator *alloc)
 {
 }
 
-int main(int argc, char **argv)
+static void vmxnet3_destructor(QOSGraphObject *obj)
 {
-    int ret;
-
-    g_test_init(&argc, &argv, NULL);
-    qtest_add_func("/vmxnet3/nop", nop);
-
-    qtest_start("-device vmxnet3");
-    ret = g_test_run();
-
-    qtest_end();
-
-    return ret;
+    QVmxnet3 *vmxnet3 = (QVmxnet3 *)obj;
+    g_free(vmxnet3);
 }
+
+static void *vmxnet3_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QVmxnet3 *vmxnet3 = g_new0(QVmxnet3, 1);
+    vmxnet3->obj.destructor = vmxnet3_destructor;
+
+    return &vmxnet3->obj;
+}
+
+static void vmxnet3_register_nodes(void)
+{
+    qos_node_create_driver("vmxnet3", vmxnet3_create);
+    qos_node_consumes("vmxnet3", "pci-bus", NULL);
+}
+
+libqos_init(vmxnet3_register_nodes);
+
+static void register_vmxnet3_test(void)
+{
+    qos_add_test("vmxnet3-test", "vmxnet3", nop, NULL);
+}
+
+libqos_init(register_vmxnet3_test);
