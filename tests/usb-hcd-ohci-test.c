3aa92203c99d73ebc56606d9540f35af0d16eee1
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-08-17 15:47:17 +0200

    test/qgraph: usb-hcd-ohci test node
    Convert tests/usb-hcd-ohci-test in qgraph test node,
    usb-hcd-ohci-test. Since it's a nop test, node creation and
    initialization is made in the same file.
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>

--- tests/usb-hcd-ohci-test.c
+++ tests/usb-hcd-ohci-test.c
@@ -10,30 +10,52 @@
 #include "qemu/osdep.h"
 #include "libqtest.h"
 #include "libqos/usb.h"
+#include "libqos/qgraph.h"
 
+typedef struct QOhci_pci QOhci_pci;
 
-static void test_ohci_init(void)
+struct QOhci_pci {
+    QOSGraphObject obj;
+};
+
+/* Tests only initialization so far. TODO: Replace with functional tests */
+static void test_ohci_init(void *obj, void *data, QGuestAllocator *alloc)
 {
-
 }
 
-static void test_ohci_hotplug(void)
+static void test_ohci_hotplug(void *obj, void *data, QGuestAllocator *alloc)
 {
     usb_test_hotplug("ohci", 1, NULL);
 }
 
-int main(int argc, char **argv)
+static void ohci_pci_destructor(QOSGraphObject *obj)
 {
-    int ret;
-
-    g_test_init(&argc, &argv, NULL);
-
-    qtest_add_func("/ohci/pci/init", test_ohci_init);
-    qtest_add_func("/ohci/pci/hotplug", test_ohci_hotplug);
-
-    qtest_start("-device pci-ohci,id=ohci");
-    ret = g_test_run();
-    qtest_end();
-
-    return ret;
+    QOhci_pci *ohci_pci = (QOhci_pci *)obj;
+    g_free(ohci_pci);
 }
+
+static void *ohci_pci_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QOhci_pci *ohci_pci = g_new0(QOhci_pci, 1);
+    ohci_pci->obj.destructor = ohci_pci_destructor;
+
+    return &ohci_pci->obj;
+}
+
+static void ohci_pci_register_nodes(void)
+{
+    qos_node_create_driver("pci-ohci", ohci_pci_create);
+    qos_node_consumes("pci-ohci", "pci-bus", &(QOSGraphEdgeOptions) {
+        .extra_device_opts = "id=ohci",
+    });
+}
+
+libqos_init(ohci_pci_register_nodes);
+
+static void register_ohci_pci_test(void)
+{
+    qos_add_test("ohci_pci-test-nop", "pci-ohci", test_ohci_init, NULL);
+    qos_add_test("ohci_pci-test-hotplug", "pci-ohci", test_ohci_hotplug, NULL);
+}
+
+libqos_init(register_ohci_pci_test);
