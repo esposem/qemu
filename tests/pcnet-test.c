391397409c4736e476cbfc7347bece1d1c672f0b
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-08-17 13:22:12 +0200

    test/qgraph: pcnet test node
    Convert tests/pcnet-test in qgraph test node,
    pcnet-test. Since it's a nop test, node creation and
    initialization is made in the same file.
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>

--- tests/pcnet-test.c
+++ tests/pcnet-test.c
@@ -9,23 +9,44 @@
 
 #include "qemu/osdep.h"
 #include "libqtest.h"
+#include "libqos/qgraph.h"
+
+typedef struct QPcnet QPcnet;
+
+struct QPcnet {
+    QOSGraphObject obj;
+};
 
 /* Tests only initialization so far. TODO: Replace with functional tests */
-static void pci_nop(void)
+static void pci_nop(void *obj, void *data, QGuestAllocator *alloc)
 {
 }
 
-int main(int argc, char **argv)
+static void pcnet_destructor(QOSGraphObject *obj)
 {
-    int ret;
-
-    g_test_init(&argc, &argv, NULL);
-    qtest_add_func("/pcnet/pci/nop", pci_nop);
-
-    qtest_start("-device pcnet");
-    ret = g_test_run();
-
-    qtest_end();
-
-    return ret;
+    QPcnet *pcnet = (QPcnet *)obj;
+    g_free(pcnet);
 }
+
+static void *pcnet_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QPcnet *pcnet = g_new0(QPcnet, 1);
+    pcnet->obj.destructor = pcnet_destructor;
+
+    return &pcnet->obj;
+}
+
+static void pcnet_register_nodes(void)
+{
+    qos_node_create_driver("pcnet", pcnet_create);
+    qos_node_consumes("pcnet", "pci-bus", NULL);
+}
+
+libqos_init(pcnet_register_nodes);
+
+static void register_pcnet_test(void)
+{
+    qos_add_test("pcnet-test", "pcnet", pci_nop, NULL);
+}
+
+libqos_init(register_pcnet_test);
