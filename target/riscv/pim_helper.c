55ec45818978edb6b64e557e9710319e986dad64
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2020-09-20 22:46:16 +0200

    rci with find_all_pages helper function

--- target/riscv/pim_helper.c
+++ target/riscv/pim_helper.c
@@ -8,6 +8,8 @@
 #include "fpu/softfloat.h"
 #include "internals.h"
 
+#define MEMSET_BYTE 1
+
 /*
     8 banks
     2^11 column per row
@@ -50,7 +52,6 @@ static void del_riscv_access(RISCVAccess *access)
     free(access->sz_pages);
 }
 
-// #if 0
 static uint32_t find_all_pages(CPURISCVState *env, RISCVAccess *access,
                            target_ulong dest, target_ulong size,
                            int mmu_idx, int ra)
@@ -64,24 +65,28 @@ static uint32_t find_all_pages(CPURISCVState *env, RISCVAccess *access,
         sz = MIN(size, -(dest | TARGET_PAGE_MASK));
 
         /*
-         * probe_access: tries to see in the TLB the hw address
-         * (that corresponds to a host pointer)
+         * tlb_vaddr_to_host: tries to see in the TLB the hw address
+         * (that corresponds to a host pointer).
+         * It's a trapless lookup, so it might return NULL if it's not
+         * found.
          */
-        printf("####Address %lu size %lu\n", (uint64_t) dest, (uint64_t) sz);
-        access->h_pages[k] = probe_write(env, dest, sz,
-                                    mmu_idx, GETPC());
+        // printf("####Address %lu size %lu\n", (uint64_t) dest, (uint64_t) sz);
+        // access->h_pages[k] = probe_write(env, dest, sz, mmu_idx, GETPC());
+        access->h_pages[k] = tlb_vaddr_to_host(env, dest, MMU_DATA_STORE, mmu_idx);
         access->addr_pages[k] = dest;
         access->sz_pages[k++] = sz;
 
         size -= sz;
         dest += sz;
+        // printf("New size is %lu\n", (uint64_t) size);
+
     }
 
     g_assert(size == 0);
 
     return k;
 }
-// #endif
+
 
 #endif
 
@@ -104,6 +109,9 @@ target_ulong helper_rcc(CPURISCVState *env, target_ulong src,
     dest_addr = g2h(dest);
     memcpy(dest_addr, src_addr, size);
 #else
+
+    RISCVAccess src_access, dest_access;
+    
     src_addr = tlb_vaddr_to_host(env, src, MMU_DATA_LOAD,
                                  cpu_mmu_index(env, 0));
     dest_addr = tlb_vaddr_to_host(env, dest, MMU_DATA_STORE,
@@ -118,8 +126,6 @@ target_ulong helper_rcc(CPURISCVState *env, target_ulong src,
 target_ulong helper_rci(CPURISCVState *env, target_ulong dest,
                         target_ulong size)
 {
-
-#define MEMSET_BYTE 1
 
     // void *dest_addr;
     target_ulong old_size = size;
@@ -145,36 +151,13 @@ target_ulong helper_rci(CPURISCVState *env, target_ulong dest,
 
     mmu_idx = cpu_mmu_index(env, false);
     oi = make_memop_idx(MO_UB, mmu_idx);
+    // printf("Size is %lu\n", (uint64_t) size);
 
     n_pages = (size / TARGET_PAGE_SIZE) + 1;
     init_riscv_access(&access, n_pages);
 
     found_pages = find_all_pages(env, &access, dest, size, mmu_idx, GETPC());
-    printf("Found %d pages\n", found_pages);
-
-    // int k = 0;
-    // target_ulong sz;
-
-    // while (size > 0) {
-    //     /* size that fits inside a page (taking into account offset) */
-    //     sz = MIN(size, -(dest | TARGET_PAGE_MASK));
-
-    //     /*
-    //      * probe_access: tries to see in the TLB the hw address
-    //      * (that corresponds to a host pointer)
-    //      */
-    //     access.h_pages[k] = probe_access(env, dest, sz, MMU_DATA_STORE,
-    //                                 mmu_idx, GETPC());
-    //     access.addr_pages[k] = dest;
-    //     access.sz_pages[k++] = sz;
-
-    //     size -= sz;
-    //     dest += sz;
-    // }
-
-    // g_assert(size == 0);
-
-    // found_pages = k;
+    // printf("Found %d pages\n", found_pages);
 
     for (int i=0; i < found_pages; i++) {
         if (likely(access.h_pages[i])) {
